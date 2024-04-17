5208cdfc435f39e7d48a03a0f379ac193c1c22c5
safa13086 <167219596+safa13086@users.noreply.github.com>
2024-04-17 13:11:09 +0530

    Add files via upload

---
+++ pracC2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+void main()
+{
+	char name[30];
+	int age;
+	char course[30];
+	printf("enter your name:\n");
+	scanf("%s",&name);
+	printf("enter your age:\n");
+	scanf("%d",&age);
+	printf("enter your course:\n");
+	scanf("%s",&course);
+	printf("your name %s\n",name);
+	printf("your age %d\n",age);
+	printf("your course %s\n",course);
+	printf("THANK YOU FOR YOUR VISIT!");
+	
+}
